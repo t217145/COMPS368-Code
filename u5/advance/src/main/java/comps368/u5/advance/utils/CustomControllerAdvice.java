697819fff6c3fb9ed09d719fea81e7bfbbcab6b9
Cyrus Cheng <cyrus@cyrus822.com>
2023-11-25 23:41:30 +0800

    bug fix

--- u5/advance/src/main/java/comps368/u5/advance/utils/CustomControllerAdvice.java
+++ u5/advance/src/main/java/comps368/u5/advance/utils/CustomControllerAdvice.java
@@ -1,16 +1,15 @@
 package comps368.u5.advance.utils;
 
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
 import comps368.u5.advance.models.CustomException;
 
 @ControllerAdvice
 public class CustomControllerAdvice {
 
     @ExceptionHandler(CustomException.class)
-    public String handleException(CustomException e, ModelMap m) {
-        m.addAttribute("ErrObject", e);
-        return "error";
+    public ModelAndView handleException(CustomException e) {
+        return new ModelAndView("error", "ErrObject", e);
     }
 }
