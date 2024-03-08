53d4f337429a591d144cc09a9f47b759e1050274
Cyrus822 <52565847+t217145@users.noreply.github.com>
2024-03-08 12:23:31 +0000

    test commit

--- u5/basic/src/main/java/comps368/u5/basic/controllers/BasicController.java
+++ u5/basic/src/main/java/comps368/u5/basic/controllers/BasicController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class BasicController {
     
+    //add a comment here
     @GetMapping({"", "/", "/index"})
     public String index(ModelMap m){
         m.addAttribute("myName", "Cyrus Cheng");
